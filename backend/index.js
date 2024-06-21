cad34fdd50120e05db885424bf8be00dcca8eac3
Ayush Bairagi <webofayush7@gmail.com>
2024-06-21 18:35:33 +0530

    Todo-List-bug-solved

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todobuddy.vercel.app",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
